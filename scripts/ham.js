3151b1cc3b9bc8e47f122d30708c7d3e9d4aa88e
PeithonKing <amukherjeeniser@gmail.com>
2021-08-06 21:23:39 +0530

    Done...

--- scripts/ham.js
+++ scripts/ham.js
@@ -1,12 +1,12 @@
-function makeCross(){
+function makeCross() {
     //console.log("Triggered")
     let top = document.getElementById("top");
     let mid = document.getElementById("mid");
     let bot = document.getElementById("bot");
     //console.log("opacity =", mid.style.opacity)
     //console.log("top transform =", top.style.transform)
-  
-    if(top.style.transform == "translateY(11px) rotate(225deg)"){
+
+    if (top.style.transform == "translateY(11px) rotate(225deg)") {
         top.style.transform = "translateY(0px) rotate(0deg)";
         top.style.backgroundColor = "#000066";
         mid.style.transform = "translateX(0px)";
@@ -14,7 +14,7 @@ function makeCross(){
         bot.style.transform = "translateY(0px) rotate(0deg)"
         bot.style.backgroundColor = "#000066";
     }
-    else{
+    else {
         top.style.transform = "translateY(11px) rotate(225deg)";
         top.style.backgroundColor = "#ff0000";
         mid.style.transform = "translateX(-200px)";
@@ -22,5 +22,4 @@ function makeCross(){
         bot.style.transform = "translateY(-11px) rotate(-225deg)"
         bot.style.backgroundColor = "#ff0000";
     }
-  
-}  
+}
